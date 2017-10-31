45b4a3e3d53f3c50ce5b2086e240a58e834d38b0
ZumiKua <wlzqkwd@Gmail.com>
2017-11-01 01:02:27 +0800

    init commit

---
+++ include/library.h
@@ -0,0 +1,6 @@
+#ifndef SHELLGHOST_LIBRARY_H
+#define SHELLGHOST_LIBRARY_H
+
+void hello(void);
+
+#endif
---
+++ sample/basic.c
@@ -0,0 +1,8 @@
+//
+// Created by ZumiKua on 2017/11/1.
+//
+#include <library.h>
+int main(){
+    hello();
+    return 0;
+}
